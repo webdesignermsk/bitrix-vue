46c4700b50076582d892aac826f41508a98bb7f2
Startsev Alexey <sa@webpractik.ru>
2019-04-22 10:27:40 +0300

    Компонент Vue: Начало

--- local/components/custom/vue-bitrix/templates/.default/script.js
+++ local/components/custom/vue-bitrix/templates/.default/script.js
@@ -1,16 +1,19 @@
 window.addEventListener('load', function () {
    BX.Vue.component('app-component', {
-       data: {
-           name: 'Андрей'
+       data: function() {
+           return  {
+               name: 'Андрей'
+           }
        },
        props: {
-           postfix: ''
+           id: Number,
+           postfix: String
        },
-       template: '<h2>Привет, {{name}}</h2>'
+       template: '<h2>Привет, {{name}}{{postfix}}</h2>'
    });
 
     BX.Vue.create({
         el: '#app',
-        template: '<app-component postfix="!"></app-component>'
+        template: '<app-component v-bind:id="1" postfix="!"></app-component>'
     })
 });
